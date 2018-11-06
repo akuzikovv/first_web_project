e15406f6f09c61e5f647941f83736e5f0198c7d7
akuzikovv@gmail.com <akuzikovv@gmail.com>
2018-11-06 18:30:29 +0200

    database

---
+++ src/main/java/com/firstwebproject/database/Main.java
@@ -0,0 +1,32 @@
+package com.firstwebproject.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static java.util.jar.Pack200.Packer.PASS;
+import static org.hibernate.cfg.AvailableSettings.USER;
+import static sun.plugin.javascript.navig.JSType.URL;
+
+public class Main {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/BD1";
+    private static final String USER = "root";
+    private static final String PASS = "root";
+
+    public static void main(String[] args) {
+        Connection connection;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASS);
+            if(!connection.isClosed()) {
+                System.out.println("Соединение с БД установлено");
+                connection.close();
+                if (connection.isClosed()){
+                    System.out.println("Соединение с БД Закрыто");
+                }
+            }
+    } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
